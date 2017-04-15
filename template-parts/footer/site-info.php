857ace80080d36620e998ad74063ba0d94bb0e4e
Annie Liu <annieliu@colourchaos.com>
2017-04-15 16:07:25 -0700

    Initial commit

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<p>Copyright &copy; 2010-<?php echo date("Y");?> ColourChaos</p>
+</div><!-- .site-info -->
